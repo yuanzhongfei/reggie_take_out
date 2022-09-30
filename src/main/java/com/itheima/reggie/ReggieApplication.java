c5b6f00fd6098577e3a1a147442aed0b3ce4dbe8
Cherish <2714703817@qq.com>
2022-09-30 21:19:31 +0800

    瑞吉外卖

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class ReggieApplication {
     public static void main(String[] args) {
-        System.out.println("fjjj");
+
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目启动成功！");
 
